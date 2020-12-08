d40666ae3218f13b0a8c1c3834506e65f8b8ee9b
brynbaxter <brynmcivor1@gmail.com>
2020-12-09 01:26:23 +1300

    update

--- 2020/8b.js
+++ 2020/8b.js
@@ -1,5 +1,5 @@
 let input = `nop +0
-acc +1
+nop +7
 jmp +4
 acc +3
 jmp -3
@@ -12,25 +12,33 @@ acc +6`;
 
 let instructList = input.split('\n');
 
-let visitedInstructions = [];
-let accumulator = 0;
-let noRepeats = true;
-let position = 0;
-let lastInstruction = instructList.length;
-console.log('lastInstruction', lastInstruction);
+for (let i = 0; i < instructList.length; i++) {
+  if (i + parseInt(instructList[i].slice(4), 10) == 596) {
+    console.log('former', i, instructList[i]);
+  } else if (i + 1 == instructList.length - 1) {
+    console.log('latter', i, instructList[i]);
+  }
+}
 
-while (position < lastInstruction) {
+let accumulator = 0;
+let position = 0;
+let lastPosition = instructList.length - 1;
+console.log('lastPosition', lastPosition, '\n');
+
+while (position <= lastPosition) {
   let instruction = instructList[position];
   let operation = instruction.slice(0, 3);
   let argument = parseInt(instruction.slice(4), 10);
+  console.log(`Position ${position} || ${operation} ${argument}`);
 
   // check if operation swap would break infinite loop
-  if (operation == 'jmp' && position + 1 >= lastInstruction) {
+  if (operation == 'jmp' && position + 1 == lastPosition) {
     operation = 'nop';
-    console.log(position);
-  } else if (operation == 'nop' && position + argument >= lastInstruction) {
+    console.log('change jmp => nop');
+  }
+  if (operation == 'nop' && position + argument == lastPosition) {
     operation = 'jmp';
-    console.log(position);
+    console.log('change nop => jmp');
   }
 
   // continue with program
@@ -42,7 +50,7 @@ while (position < lastInstruction) {
   } else {
     position++;
   }
-  console.log(`${instruction} ${argument} => ${position}`);
+  console.log(` => ${position}\n`);
 }
 
 console.log(accumulator);
