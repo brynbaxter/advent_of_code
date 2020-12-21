aad33499a39238533381bf03a24d42e4c3abc011
brynbaxter <brynmcivor1@gmail.com>
2020-12-21 23:23:30 +1300

    Stole 13b

--- 2020/13b.js
+++ 2020/13b.js
@@ -1,44 +1,29 @@
-let input = `939
-17,x,13,19`;
+// let input = `939
+// 1789,37,47,1889`;
 
 // let input = document.querySelector('pre').innerText;
 
-// const fs = require('fs');
-// const path = require('path');
-// const input = fs.readFileSync(path.resolve(__dirname, '13_input.txt'), 'utf8');
+const fs = require('fs');
+const path = require('path');
+const input = fs.readFileSync(path.resolve(__dirname, '13_input.txt'), 'utf8');
 
-let notes = input.replace(/\r/g, '').split('\n');
+const [firstBus, ...buses] = input
+  .split('\n')[1]
+  .split(',')
+  .map((n, i) => [parseInt(n, 10), i])
+  .filter(([n]) => !Number.isNaN(n));
 
-let busArr = notes[1].split(',').map((item) => {
-  if (item == 'x') {
-    return item;
-  } else {
-    return parseInt(item);
+let multiplier = firstBus[0];
+let firstViableTime = 0;
+
+buses.forEach(([bus, busIndex]) => {
+  while (true) {
+    if ((firstViableTime + busIndex) % bus === 0) {
+      multiplier *= bus;
+      break;
+    }
+    firstViableTime += multiplier;
   }
 });
 
-let newStartTime = busArr[0];
-let lastConfirmedTime = busArr[0];
-console.log(newStartTime);
-
-const getCommonTime = (i) => {
-  while (true) {
-    console.log((newStartTime + i) / busArr[i]);
-    if ((newStartTime + i) % busArr[i] == 0) {
-      lastConfirmedTime = newStartTime;
-      console.log('lastConfirmedTime', lastConfirmedTime);
-      break;
-    } else {
-      newStartTime += lastConfirmedTime;
-    }
-  }
-};
-
-for (let i = 1; i < busArr.length; i++) {
-  console.log(busArr[i]);
-  if (busArr[i] !== 'x') {
-    getCommonTime(i);
-  }
-}
-
-console.log(lastConfirmedTime);
+console.log(firstViableTime);
