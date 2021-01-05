cec7e24d4f5f1bd49542b4dc4a36cd714d8cba07
Bryn <brynmcivor1@gmail.com>
2021-01-05 19:03:52 +1300

    beign day 14 part 2

--- 2020/14.js
+++ 2020/14.js
@@ -1,14 +1,13 @@
-// let input = `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-// mem[8] = 11
-// mem[7] = 101
-// mem[8] = 0
-// `;
+let input = `mask = 00X1001X
+mem[42] = 100
+mask = 0000X0XX
+mem[26] = 1`;
 
 // let input = document.querySelector('pre').innerText;
 
-const fs = require('fs');
-const path = require('path');
-const input = fs.readFileSync(path.resolve(__dirname, '14_input.txt'), 'utf8');
+// const fs = require('fs');
+// const path = require('path');
+// const input = fs.readFileSync(path.resolve(__dirname, '14_input.txt'), 'utf8');
 
 let commands = input
   .replace(/\r/g, '')
@@ -17,7 +16,7 @@ let commands = input
     return x.split(' = ');
   });
 
-const applyBitmask = (integer, bitmask) => {
+const applyBitmaskV1 = (integer, bitmask) => {
   let bitString = Math.abs(integer).toString(2);
   while (bitString.length < bitmask.length) {
     bitString = '0' + bitString;
@@ -54,7 +53,7 @@ const partOne = () => {
       bitmask = x[1];
     } else {
       let memPosition = x[0].slice(4, -1);
-      let newValue = applyBitmask(x[1], bitmask);
+      let newValue = applyBitmaskV1(x[1], bitmask);
       memory[memPosition] = newValue;
     }
   });
@@ -62,4 +61,35 @@ const partOne = () => {
   return sum;
 };
 
-console.log(partOne());
+const getMappedFloatingBits = (bitmask) => {
+  let answerArr = [];
+  console.log(bitmask);
+  let countX = bitmask.split('X').length - 1;
+  for (let i = 0; i < countX; i++) {
+    let bitString = i.toString(2);
+    while (bitString.length < countX.toString(2).length) {
+      bitString = '0' + bitString;
+    }
+    let bitArray = Array.from(bitString);
+    console.log(countX, bitString);
+  }
+};
+
+const partTwo = () => {
+  let memory = [];
+  let bitmask = '';
+  commands.forEach((command) => {
+    if (command[0] == 'mask') {
+      bitmask = command[1];
+    } else {
+      let memPosition = command[0].slice(4, -1);
+      let mappedFloatingBits = getMappedFloatingBits(bitmask);
+    }
+  });
+  let sum = sumMemory(memory);
+  return sum;
+};
+
+console.log('part one', partOne());
+
+console.log('part two', partTwo());
